9468bd10a5089df530d4370328cae479aaf7254e
Mauro P <maurop123@gmail.com>
2024-09-09 03:14:38 -0600

    router working for real

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,19 @@
 import React from 'react'
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { changeVideosPage, getVideosFetch } from './store/actions/videosAction.ts'
-import './App.css'
-import Tile from './components/TileComponent'
+import { Routes, Route, Outlet } from 'react-router-dom'
+import HomePage from './pages/HomePage.tsx'
 
-const HomePage = () => {
-  const [count, setCount] = useState(0)
-  const dispatch = useDispatch()
-  const videos = useSelector(state => state.videos.videos)
-  const videosPage = useSelector(state => state.videos.page)
-  const videosPageSize = useSelector(state => state.videos.pageSize)
-  const videosPages = Math.ceil(videos.length / videosPageSize)
-
+export default function App() {
   return (
-    <div className="h-full flex flex-col justify-between">
-      <h1>Get Hyped Music Videos</h1>
-      <div className="card flex flex-wrap justify-center">
-      { videos.length > 0
-        ?
-          <>
-            {
-              videos
-              .slice((videosPage-1)*videosPageSize, videosPage*videosPageSize)
-              .map(video => {
-                return (
-                  <Tile key={video.videoId} videoId={video.videoId} title={video.title} />
-                )
-              })
-            }
-            <div className="w-full">
-              {
-                Array(videosPages).fill().map((x,i) => {
-                  return (<button key={i} className="m-2 rounded-full" onClick={() => dispatch(changeVideosPage(i+1))}>
-                  {i+1}
-                  </button>)
-                })
-              }
-            </div>
-          </>
-        :
-          <button onClick={() => dispatch(getVideosFetch())}>
-            Get Music Videos 
-          </button>
-      }
-      </div>
-      <p className="read-the-docs">
-      This is an app for getting hyped with your favorite videos
-      </p>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+    </Routes>
   )
 }
 
-export default HomePage
+function Layout() {
+  return (
+    <Outlet />
+  )
+}
